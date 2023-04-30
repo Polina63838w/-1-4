44da168f01a0072f8ccaaac0869ef6c9c4fe98f5
Polina63838w <119042408+Polina63838w@users.noreply.github.com>
2023-04-30 14:37:45 +1000

    Add files via upload

--- task1.java
+++ task1.java
@@ -1,23 +1,20 @@
-package For_java_HW;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class task1{
     
-
     public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map){
         if (map.containsKey(key)) {
             map.get(key).add(value);
- } else {
+        } else {
             ArrayList<Integer> list = new ArrayList<>();
             list.add(value);
             map.put(key, list);
         }
 
     }
-    
+
     public static void printBook(Map<String, ArrayList<Integer>> map){
         for (var item : map.entrySet()) {
             String phones = "";
@@ -29,12 +26,12 @@ public class task1{
     }
     public static void main(String[] args) {
         Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
-        addNumber("Morozov", 749390, bookPhone);
-        addNumber("Morozov", 4593386, bookPhone);
-        addNumber("Petrov", 926549, bookPhone);
-        addNumber("Popov", 956875734, bookPhone);
-        addNumber("Popov", 48363842, bookPhone);
-        addNumber("Dobro", 9787667, bookPhone);
+        addNumber("Ivanov", 123, bookPhone);
+        addNumber("Ivanov", 1234, bookPhone);
+        addNumber("Petrov", 546585, bookPhone);
+        addNumber("Sidorov", 8956477, bookPhone);
+        addNumber("Ivanov", 12356233, bookPhone);
+        addNumber("Petrov", 787897, bookPhone);
         printBook(bookPhone);
        }
 }
